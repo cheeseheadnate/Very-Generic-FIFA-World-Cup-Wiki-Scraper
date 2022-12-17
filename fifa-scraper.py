13ae162141860c1db0e83065bcb388ff6d953e89
cheeseheadnate <cheeseheadnate@gmail.com>
2022-12-17 06:05:52 -0600

    fifa-scraper.py added to repo

---
+++ fifa-scraper.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+# Based on Wikipedia for educational purposes
+
+years = [1930, 1934, 1938, 1950, 1954, 1958, 1962, 1966, 1970, 1974, 1978, 1982, 1986, 1990, 1994, 1998, 2002, 2006, 2010, 2014, 2018]
+
+def get_matches(year):
+   web = f'https://en.wikipedia.org/wiki/{year}_FIFA_World_Cup'
+   response = requests.get(web)
+   content = response.text
+   soup = BeautifulSoup(content, 'lxml')
+
+   matches = soup.find_all('div', class_='footballbox')
+
+   home = []
+   score = []
+   away = []
+
+   for match in matches:
+      home.append(match.find('th', class_='fhome').get_text())
+      score.append(match.find('th', class_='fscore').get_text())
+      away.append(match.find('th', class_='faway').get_text())
+
+   dict_football = { 'home': home , 'score': score , 'away': away }
+
+   footballDF = pd.DataFrame(dict_football)
+   footballDF['year'] = year
+   return footballDF
+
+# Enter year here
+print(get_matches('1950'))
